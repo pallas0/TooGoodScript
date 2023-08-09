474e6c316230e89a54f7bde219987a83c59a0fad
Amelia Risner <amelia.risner0@gmail.com>
2023-08-09 11:56:56 -0700

    framework switched to Django, models class defined

---
+++ TooGood_Script/toogood/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ToogoodConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'toogood'
--- app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, request, jsonify
-import requests
-from twilio.rest import Client
-
-app = Flask(__name__)
