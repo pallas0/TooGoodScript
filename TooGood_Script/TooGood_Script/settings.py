93813afe71e4b798fc0d9ad2d31471bcb0b55df4
Amelia Risner <amelia.risner0@gmail.com>
2023-08-10 12:48:39 -0700

    stumbling through framework stuff

--- TooGood_Script/TooGood_Script/settings.py
+++ TooGood_Script/TooGood_Script/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'toogood.apps.ToogoodConfig' #added per youtube vid
 ]
 
 MIDDLEWARE = [
--- TooGood_Script/toogood/views.py
+++ TooGood_Script/toogood/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
+#third party API calls go here
