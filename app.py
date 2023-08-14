f1301d48688afb2a810d79866b7e1107caf7cba0
Amelia Risner <amelia.risner0@gmail.com>
2023-08-14 11:58:23 -0700

    database created, route added to see database

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Replace 'your_database_uri' with the actual connection URI to your PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ameliarisner:123@localhost:5432/toogood'
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/users')
+def list_users():
+    users = User.query.all()
+    return '\n'.join([f"{user.id}: {user.email}, {user.phone_number}" for user in users])
