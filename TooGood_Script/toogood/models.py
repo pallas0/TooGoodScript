7490b6f139708f84842482122d82345f64542fac
Amelia Risner <amelia.risner0@gmail.com>
2023-08-14 10:53:48 -0700

    well about to change all of this anyway

--- TooGood_Script/toogood/models.py
+++ TooGood_Script/toogood/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class FavoriteRestaurant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.Cascade)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant_id = models.CharField(max_length=100)
     restaurant_name = models.CharField(max_length=200)
     has_bags_available = models.BooleanField(default=False)
--- TooGood_Script/toogood/views.py
+++ TooGood_Script/toogood/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from config import client
+import requests
 
 # Create your views here.
 #third party API calls go here
+items = client.get_items()
+print(items)
