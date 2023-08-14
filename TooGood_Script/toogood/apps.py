f5b0f93756c5f474446c4231a4f2e571a7c4960e
Amelia Risner <amelia.risner0@gmail.com>
2023-08-14 11:04:43 -0700

    Flask bare bones working

--- TooGood_Script/toogood/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToogoodConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'toogood'
--- TooGood_Script/toogood/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from config import client
-import requests
-
-# Create your views here.
-#third party API calls go here
-items = client.get_items()
-print(items)
