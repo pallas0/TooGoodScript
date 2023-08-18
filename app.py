de7e3a77dfa43b4326654ed9192c86044c6515e3
Amelia Risner <amelia.risner0@gmail.com>
2023-08-18 16:45:36 -0700

    db separated into models file, working

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
+import os
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from tgtg import TgtgClient
 
+from models import db, Subscriber, Credential, Favorite
+
+DATABASE_URI = os.environ.get("DATABASE_URI")
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ameliarisner:123@localhost:5432/toogood'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
-#database creation and set up
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db.init_app(app)
 
-class Subscriber(db.Model):
-    __tablename__ = 'subscriber'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone_number = db.Column(db.String(20), unique=True, nullable=False)
-
-class Credential(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    access_token = db.Column(db.String(255), nullable=False)
-    refresh_token = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, unique=True, nullable=False)
-    cookie = db.Column(db.String(255))
-    subscriber_id = db.Column(db.Integer, db.ForeignKey('subscriber.id'), unique=True, nullable=False)
-    subscriber = db.relationship('Subscriber', backref='credential', uselist=False)
-
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    new_bags = db.Column(db.Boolean, nullable=False)
-    subscriber_id = db.Column(db.Integer, db.ForeignKey('subscriber.id'), nullable=False)
-    subscriber = db.relationship('Subscriber', backref='favorites')
 
 #manually added for testing purposes
 # with app.app_context():
-#     new_subscriber = Subscriber(email='amelia.risner0@gmail.com', phone_number='4076339712')
+#     new_subscriber = Subscriber(email='example@gmail.com', phone_number='example')
 #     db.session.add(new_subscriber)
 #     db.session.commit()
         
---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+class Subscriber(db.Model):
+    __tablename__ = 'subscriber'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+
+class Credential(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    access_token = db.Column(db.String(255), nullable=False)
+    refresh_token = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    cookie = db.Column(db.String(255))
+    subscriber_id = db.Column(db.Integer, db.ForeignKey('subscriber.id'), unique=True, nullable=False)
+    subscriber = db.relationship('Subscriber', backref='credential', uselist=False)
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    new_bags = db.Column(db.Boolean, nullable=False)
+    subscriber_id = db.Column(db.Integer, db.ForeignKey('subscriber.id'), nullable=False)
+    subscriber = db.relationship('Subscriber', backref='favorites')
