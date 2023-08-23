a6bbedcf04d1488ab278782acd51b7f17efdc991
Amelia Risner <amelia.risner0@gmail.com>
2023-08-23 15:29:34 -0700

    omg YES it stored it correctl yesssssssssss

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db.init_app(app)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
+#temporarily leaving this here 
+# def clear_table(table):
+#     db.session.query(table).delete()
+#     db.session.commit()
+    
+# with app.app_context():
+#     clear_table(Subscriber)
 
 
 #route to see database (for debug purposes)
@@ -23,6 +30,11 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 def list_subscribers():
     subscribers = Subscriber.query.all()
     return '\n'.join([f"{subscriber.id}: {subscriber.email}, {subscriber.phone_number}" for subscriber in subscribers])
+
+@app.route('/credentials')
+def list_credentials():
+    credentials = Credential.query.all()
+    return '\n'.join([f"{credential.id}: {credential.access_token}, {credential.refresh_token}, {credential.user_id}, {credential.cookie}, {credential.subscriber_id}" for credential in credentials])
 
 @app.route('/submit_subscriber_info', methods=['POST'])
 def submit_subscriber_info():
@@ -36,7 +48,17 @@ def submit_subscriber_info():
 
     email = new_subscriber.email
     client = TgtgClient(email=email)
-    credentials = client.get_credentials()
-    #print(credentials)
+    credentials_data = client.get_credentials()
+
+    credential = Credential(
+        access_token=credentials_data['access_token'],
+        refresh_token=credentials_data['refresh_token'],
+        user_id=credentials_data['user_id'],
+        cookie=credentials_data['cookie'],
+        subscriber_id=new_subscriber.id  # Assuming subscriber.id is the ID of the newly added subscriber
+    )
+
+    db.session.add(credential)
+    db.session.commit()
 
     return jsonify({'message': 'Subscriber information added successfully'})
