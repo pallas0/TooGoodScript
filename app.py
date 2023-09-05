6d3dd822d8d43db34d07f53321de2513b0dd25bc
Amelia Risner <amelia.risner0@gmail.com>
2023-09-04 17:47:14 -0700

    test data removed

--- app.py
+++ app.py
@@ -47,14 +47,9 @@ def get_user_items(subscriber):
 
 @app.route('/favorites/availability')
 def check_if_favorites_available():
-    test_data = [{'display_name': 'Obour Foods (Hummus & Toum)', 'items_available': 10},
-                 {'display_name': "Ha Tea - Chinatown (Fruits)", 'items_available': 1},
-                 {'display_name': 'Gracias Madre (Surprise Bag)', 'items_available': 0},
-                 {'display_name': 'Mission Minis', 'items_available': 0}]
     subscribers = Subscriber.query.all()
     for subscriber in subscribers:
-        #items = get_user_items(subscriber)
-        items = test_data
+        items = get_user_items(subscriber)
         if not items:
             return f"No items found for user {subscriber.id}, 400"
         for item in items:
